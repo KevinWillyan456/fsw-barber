1d3e31196a9ad160b6d49325b2adaba469c32b17
Kevin Souza <kevinwillyansouza@gmail.com>
2024-12-31 17:52:39 -0400

    feat: adicionar componente de reservas inicial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import BookingItem from '../_components/booking-item'
 import Header from '../_components/header'
 import Search from './_components/search'
 
@@ -17,8 +18,15 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="p-5 mt-6">
+            <div className="px-5 mt-6">
                 <Search />
+            </div>
+
+            <div className="px-5 mt-6">
+                <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+                    Agendamentos
+                </h2>
+                <BookingItem />
             </div>
         </div>
     )
