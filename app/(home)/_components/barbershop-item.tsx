2359e6b9efa9ddbcddc2d92a141cd1d64ea3622c
Kevin Souza <kevinwillyansouza@gmail.com>
2025-01-01 15:35:13 -0400

    feat: adicionar componente de barbearia inicial

---
+++ app/(home)/_components/barbershop-item.tsx
@@ -0,0 +1,52 @@
+import { Badge } from '@/app/_components/ui/badge'
+import { Button } from '@/app/_components/ui/button'
+import { Card, CardContent } from '@/app/_components/ui/card'
+import { BarberShop } from '@prisma/client'
+import { StarIcon } from 'lucide-react'
+import Image from 'next/image'
+
+interface BarbershopItemProps {
+    barbershop: BarberShop
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+    return (
+        <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+            <CardContent className="px-1 py-0">
+                <div className="relative h-[159px] w-full">
+                    <div className="absolute top-2 left-2 z-50">
+                        <Badge
+                            variant="secondary"
+                            className="opacity-90 flex gap-1 items-center"
+                        >
+                            <StarIcon
+                                size={12}
+                                className="fill-primary text-primary"
+                            />
+                            <span className="text-sx">5,0</span>
+                        </Badge>
+                    </div>
+                    <Image
+                        src={barbershop.imageUrl}
+                        alt={barbershop.name}
+                        fill
+                        className="rounded-2xl object-cover"
+                    />
+                </div>
+
+                <div className="px-2 pb-3">
+                    <h2 className="font-bold mt-2 truncate">
+                        {barbershop.name}
+                    </h2>
+                    <p className="text-sm text-gray-400 truncate">
+                        {barbershop.address}
+                    </p>
+                    <Button className="w-full mt-3" variant="secondary">
+                        Reservar
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+export default BarbershopItem
