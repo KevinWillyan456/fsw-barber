7d5fef7932c0e1f7074d92c4b3f5d60622cc0cf6
Kevin Souza <kevinwillyansouza@gmail.com>
2025-01-02 16:28:02 -0400

    feat: adicionar componente footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,13 @@
-import { format } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
-import BookingItem from '../_components/booking-item'
-import Header from '../_components/header'
-import { db } from '../_lib/prisma'
-import BarbershopItem from './_components/barbershop-item'
-import Search from './_components/search'
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import BookingItem from '../_components/booking-item';
+import Header from '../_components/header';
+import { db } from '../_lib/prisma';
+import BarbershopItem from './_components/barbershop-item';
+import Search from './_components/search';
 
 export default async function Home() {
-    const barbershops = await db.barberShop.findMany()
+    const barbershops = await db.barberShop.findMany();
 
     return (
         <div>
@@ -47,6 +47,21 @@ export default async function Home() {
                     ))}
                 </div>
             </div>
+
+            <div className="mt-6 mb-[4.5rem]">
+                <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+                    Populares
+                </h2>
+
+                <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                    {barbershops.map((barbershop) => (
+                        <BarbershopItem
+                            key={barbershop.id}
+                            barbershop={barbershop}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
